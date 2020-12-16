127843536464ee3f667514580feb8e53fdd050e0
Robert Owens <raowens15@icloud.com>
2020-12-15 19:29:03 -0500

    Finished the todolist tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react"
 import './App.css';
+//Importing Components
+import Form from './components/Forms'
+import TodoList from './components/TodoList'
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  useEffect(() => {
+    if(localStorage.getItem("todos") === null){
+      localStorage.setItem("todos", JSON.stringify([]));
+    }
+    else{
+      setTodos(JSON.parse(localStorage.getItem((("todos")))));
+    }
+  }, []);
+  useEffect(() => {
+    switch(status){
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;  
+    }
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos, status]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Robbie's Todo List</h1>
       </header>
+      <Form
+        todos={todos} 
+        setTodos={setTodos}
+        inputText={inputText} 
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList
+        filteredTodos={filteredTodos}
+        setTodos={setTodos}
+        todos={todos}
+      />
     </div>
   );
 }
