6ca7f6a2237e8fc50f3b5f83c4138b06d9c59271
Robert Owens <raowens15@icloud.com>
2020-12-15 20:51:35 -0500

    Added an additional comment for clarity

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,10 +3,10 @@ import Todo from "./Todo"
 
 const TodoList = ({ todos, setTodos, filteredTodos }) => {
     //Renders the list of todos
+    //chooses which todos are rendered based on status state
     return (
         <div className="todo-container">
             <ul className="todo-list">
-                //chooses which todos are rendered based on status state
                 { filteredTodos.map((todo) => (
                     <Todo
                         setTodos={setTodos}
